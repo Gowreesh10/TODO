ee17fc2e7de33cd8acef89aed8796ef463ae487b
Gowreesh10 <gowreeshcse10@gmail.com>
2024-07-31 13:10:26 +0530

    starter-things

---
+++ TODO-FE/todo-fe/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import LandingPage from './Components/LandingPage/LandingPage';
+function App() {
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
