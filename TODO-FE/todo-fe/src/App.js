bdabd457fa090a066fdc40856c224ef48572ba74
Gowreesh10 <gowreeshcse10@gmail.com>
2024-08-01 01:03:59 +0530

    Landing page completed

--- TODO-FE/todo-fe/src/App.js
+++ TODO-FE/todo-fe/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 import LandingPage from './Components/LandingPage/LandingPage';
+import AuthPage from './Components/AuthPage/AuthPage';
 function App() {
   return (
     <div>
-      <LandingPage />
+      <Router>
+        <Routes>
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/auth' element={<AuthPage />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- TODO-FE/todo-fe/src/Components/LandingPage/LandingPage.jsx
+++ TODO-FE/todo-fe/src/Components/LandingPage/LandingPage.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
-import './LandingPage'
-
+import './LandingPage.css'
+import { useNavigate } from 'react-router-dom'
 function LandingPage() {
+    const navigate = useNavigate();
+
+    const handleGetStarted = ()=>{
+        navigate('/auth');
+    }
   return (
-    <div>LandingPage</div>
+    <div className='container'>
+        <body>
+            <header>
+                <h1 className='fredericka-the-great-regular'>TODO</h1>
+            </header>
+            <main>
+                <h4 className='kaushan-script-regular'>Stay Organized and On Track,</h4>
+                <p className='gentium-book-plus-regular'>Never miss a deadline or forget an important task again.</p>
+                <p className='gentium-book-plus-regular'>Our app helps you stay organized, so you can focus on what matters most.</p>
+                <div className="button-container"><button className='button' onClick={handleGetStarted}>Get Started</button></div>
+            </main>
+        </body>
+    </div>
   )
 }
 
